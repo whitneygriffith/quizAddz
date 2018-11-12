c5249c8ffba8c631b60122f899b735ed8cb539a0
Whitney Griffith <whitney.griffith@bison.howard.edu>
2018-11-11 20:22:10 -0500

    ignore *.pyc

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField, PasswordField, SubmitField
-from wtforms import ValidationError, validators
-
-class Login(Form):
-    email = TextField('Username', [validators.Required("Please enter your email."), validators.Email("Please enter your email.")])
-    password = PasswordField('Password', [validators.Required("Please enter your password"), validators.Length(min=4, max=15)])
-    submit = SubmitField("Enter")
